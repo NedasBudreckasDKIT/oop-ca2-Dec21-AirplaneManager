cdbcef4663d225281d111978e3c8feefba61912e
ned <d00238459@student.dkit.ie>
2021-12-15 13:12:59 +0000

    update

--- src/main/java/dkit/oop/PassengerAirplane.java
+++ src/main/java/dkit/oop/PassengerAirplane.java
@@ -7,16 +7,23 @@ import java.util.Scanner;
 public class PassengerAirplane extends Airplane {
 //    public static Scanner input = App.scanner;
 //    // fields(Q2)
-//    int MAX_NUM_PASSENGERS;
-//    String passenger;
-//    public static ArrayList passengers;
-//
-//
-
-//    PassengerAirplane(String type, int maxNumPassengers) {
+//    public PassengerAirplane(String type, int MAX_NUM_PASSENGERS) {
 //        super(type);
+//        this.MAX_NUM_PASSENGERS = MAX_NUM_PASSENGERS;
+//    }
 //
+//    public PassengerAirplane(String type, int MAX_NUM_PASSENGERS, ArrayList<String> passengerList) {
+//        super(type);
+//        this.MAX_NUM_PASSENGERS = MAX_NUM_PASSENGERS;
+//        this.passengerList = passengerList;
+//    }
 //
+//    @Override
+//    public String toString() {
+//        return "PassengerAirplane{" +
+//                "MAX_NUM_PASSENGERS=" + MAX_NUM_PASSENGERS +
+//                ", passengerList=" + passengerList +
+//                '}' + super.toString();
 //    }
 //
 //    public void addPassenger(String name) {
