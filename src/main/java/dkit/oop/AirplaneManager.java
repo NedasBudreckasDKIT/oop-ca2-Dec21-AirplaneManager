f2a47cea636f9d4a6d55706625d173acd84bb9a2
ned <d00238459@student.dkit.ie>
2021-12-15 12:58:40 +0000

    update

--- src/main/java/dkit/oop/AirplaneManager.java
+++ src/main/java/dkit/oop/AirplaneManager.java
@@ -1,9 +1,7 @@
 package dkit.oop;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * AirplaneManager is a container class that stores Airplanes
@@ -23,39 +21,37 @@ public class AirplaneManager {
     }
 
     //Q3.
-
-    // write add() method
-public void add(Airplane airplane){
-    System.out.println("Airplane Type: ");
-     String type = input.nextLine();
-    System.out.println("Airplane Max Load(KG): ");
-    int MAX_LOAD_KG = input.nextInt();
-    Airplane plane = new Airplane(type,MAX_LOAD_KG);
-    airplaneList.add(plane);
-}
-
-    public void displayAllAirplanes() {
-        for (Airplane airplane: airplaneList){
-            System.out.println(airplane);
-        }
-    }
-
-    public void displayAllPassengerAirplanes(String type) {
-        if (type.equals("passengerPlane")){
-            ArrayList<Airplane> airplaneList = sort(type);
-            for(Airplane airplane : new Airplane){
-                System.out.println(airplane);
-            }
-        }
-    }
-    public void getAllCargoAirplanes(String type) {
-        if (type.equals("cargoPlane")){
-            ArrayList<Airplane> airplaneList = sort(type);
-            for(Airplane airplane : airplane){
-                System.out.println(airplane);
-            }
-        }
-    }
+//public void add(Airplane airplane){
+//    System.out.println("Airplane Type: ");
+//     String type = input.nextLine();
+//    System.out.println("Airplane Max Load(KG): ");
+//    int MAX_LOAD_KG = input.nextInt();
+//    Airplane plane = new Airplane(type,MAX_LOAD_KG);
+//    airplaneList.add(plane);
+//}
+//
+//    public void displayAllAirplanes() {
+//        for (Airplane airplane: airplaneList){
+//            System.out.println(airplane);
+//        }
+//    }
+//
+//    public void displayAllPassengerAirplanes(String type) {
+//        if (type.equals("passengerPlane")){
+//            ArrayList<Airplane> airplaneList = sort(type);
+//            for(Airplane airplane : new Airplane){
+//                System.out.println(airplane);
+//            }
+//        }
+//    }
+//    public void getAllCargoAirplanes(String type) {
+//        if (type.equals("cargoPlane")){
+//            ArrayList<Airplane> airplaneList = sort(type);
+//            for(Airplane airplane : airplane){
+//                System.out.println(airplane);
+//            }
+//        }
+//    }
 //    public void addPassengerNameToAirplane( airplaneId, passengerName){
 //
 //    }
@@ -64,9 +60,22 @@ public void add(Airplane airplane){
 //
 //    }
 
-     public void displayAllAirplanesInOrderOfType( ){
-
-     }
+//     public void displayAllAirplanesInOrderOfType(String type ){
+//         ArrayList<Airplane> airplanes= new ArrayList<>();
+//         for(Airplane airplane : airplane){
+//             if(airplane.getType().equals(type)){
+//                 airplanes.add(airplane);
+//             }
+//         }
+//         Collections.sort(airplane, new Comparator<Airplane>(){
+//             @Override
+//             public int compare(Airplane o1, Airplane o2){
+//                 int result = o1.getType().compareTo(o2.getType());
+//                 return result;
+//             }
+//
+//         }
+//     }
 
 
 } // end of AirplaneManager
--- src/main/java/dkit/oop/PassengerAirplane.java
+++ src/main/java/dkit/oop/PassengerAirplane.java
@@ -5,28 +5,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class PassengerAirplane extends Airplane {
-    public static Scanner input = App.scanner;
-    // fields(Q2)
-    int MAX_NUM_PASSENGERS;
-    String passenger;
-    public static ArrayList passengers;
+//    public static Scanner input = App.scanner;
+//    // fields(Q2)
+//    int MAX_NUM_PASSENGERS;
+//    String passenger;
+//    public static ArrayList passengers;
+//
+//
 
-
-
-    PassengerAirplane(String type, int maxNumPassengers) {
-        super(type);
-
-
-    }
-
-    public void addPassenger(String name) {
-        System.out.println("Enter name: ");
-        name = input.nextLine();
-
-    }
-    @Override
-    public String toString() {
-        return ""
-    }
+//    PassengerAirplane(String type, int maxNumPassengers) {
+//        super(type);
+//
+//
+//    }
+//
+//    public void addPassenger(String name) {
+//        System.out.println("Enter name: ");
+//        name = input.nextLine();
+//
+//    }
+//    @Override
+//    public String toString() {
+//        return "Name: "+
+//    }
 
 } // end of PassengerAirplane
